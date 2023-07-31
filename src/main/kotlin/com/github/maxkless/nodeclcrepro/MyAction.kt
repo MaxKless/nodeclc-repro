f3d59c4881f5f3c9bdb62619303d75fcbc77bb10
GitHub Action <action@github.com>
2023-07-31 10:11:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxkless/nodeclcrepro/MyAction.kt
+++ src/main/kotlin/com/github/maxkless/nodeclcrepro/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.maxkless.nodeclcrepro
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/maxkless/nodeclcrepro/MyBundle.kt
+++ src/main/kotlin/com/github/maxkless/nodeclcrepro/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.maxkless.nodeclcrepro
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
